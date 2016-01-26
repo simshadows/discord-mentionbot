e1180a4b8893490bf4e3f8ff307119047fe0c279
Rhomil Cabanilla <rhomilc@hotmail.com>
2016-01-26 21:14:47 +1100

    Moved MentionSummaryCache into its own source file.

--- mentionbot.py
+++ mentionbot.py
@@ -10,6 +10,7 @@ import os
 import random
 
 import clientextended
+import mentionsummarycache
 
 LOGIN_DETAILS_FILENAME = "login_details" # This file is used to login. Only contains two lines. Line 1 is email, line 2 is password.
 MESSAGE_MAX_LEN = 2000
@@ -53,7 +54,7 @@ def initialize_global_variables():
    global botowner_mention
    global botowner
    global initialization_timestamp
-   mentionSummaryCache = MentionSummaryCache()
+   mentionSummaryCache = mentionsummarycache.MentionSummaryCache()
    bot_mention = "<@{}>".format(client.user.id)
    bot_name = client.user.name
    botowner_mention = "<@{}>".format(BOTOWNER_ID)
@@ -526,78 +527,6 @@ def simple_easter_egg_replies(msg):
    return
 
 
-# TODO: Replace with better data structure, or integrate it into MentionLogger.
-#       This version is just horribly inefficient linear searching,
-#       , and operates on messages directly.
-class MentionSummaryCache:
-   
-   def __init__(self):
-      self._mention_list = [] # FORMAT: list<tuple<userID, list<messageObject> >>
-      print("MentionSummaryCache initialized.")
-      return
-
-   # Adds a message to the collection (and removes redundant data).
-   def add_message(self, msg):
-
-      # Get all users mentioned.
-      user_ids_mentioned = [] # TYPE: List<String>
-      for user in msg.mentions:
-         user_ids_mentioned.append(user.id)
-
-      for user_mentioned in user_ids_mentioned:
-         
-         # Check if the message's sender is in the list
-         user_tuple = None # FORMAT: Tuple<userID, List<messageObject> >
-         for j in self._mention_list:
-            if j[0] == user_mentioned:
-               user_tuple = j
-               break
-         
-         # If user needs to be added to the list, add them.
-         # If they're already on the list, check if there's already a message from the current channel.
-         if user_tuple == None:
-            user_tuple = (user_mentioned, [])
-            self._mention_list.append(user_tuple)
-         else:
-            for i in user_tuple[1]:
-               if i.channel.id == msg.channel.id:
-                  user_tuple[1].remove(i)
-                  print("An older MentionSummaryCache entry was removed!")
-                  break
-
-         # Append new message
-         user_tuple[1].append(msg)
-
-      return
-
-   # Clears data about a single user from the collection.
-   def delete_user_data(self, user_ID):
-      for i in self._mention_list:
-         if i[0] == user_ID:
-            self._mention_list.remove(i)
-            break
-      return
-
-   # Gets all messages for a particular user.
-   def get_user_latest(self, user_ID): # FORMAT: list<messageObject>
-      user_message_list = [] # FORMAT: tuple<userID, list<messageObject> >
-      for i in self._mention_list:
-         if i[0] == user_ID:
-            user_message_list = i[1]
-            break
-      return user_message_list
-
-   # Output is not NL-terminated.
-   def user_data_to_string(self, user_ID, verbose=False): # TYPE: String
-
-      return msg_list_to_string(self.get_user_latest(user_ID), verbose=verbose)
-   def user_has_mentions(self, user_ID): #TYPE: Boolean
-      for i in self._mention_list:
-         if i[0] == user_ID:
-            return True
-      return False
-
-
 def msg_list_to_string(mentions, verbose=False): # TYPE: String
    now = datetime.datetime.now()
    buf = "" # FORMAT: String
@@ -679,5 +608,3 @@ def get_bot_uptime():
 
 
 client.run() # let the games begin...
-
-
