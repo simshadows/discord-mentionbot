a2361c064a122bd53fe986463ceecdfd27f5b74c
Rhomil Cabanilla <rhomilc@hotmail.com>
2016-05-15 07:57:16 +1000

    The greeting template is now persistently stored in the PM Greetings module.

--- mentionbot/servermodules/pmgreetings.py
+++ mentionbot/servermodules/pmgreetings.py
@@ -27,18 +27,40 @@ See `{modhelp}` to manage the new-member PM greeting system.
 
    _SUBSTITUTION_INFO = textwrap.dedent("""
       When setting the greeting message, substitutions can be made.
-      `${0}` substitutes into the member's name, and `${1}` substitutes into the server's name.
+      `${0}` substitutes into a mention of the new member, and `${1}` substitutes into the server's name.
       
       Escaping: If you want to add a literal `${0}`, you can escape `$` by with `$${0}`. Similarly is done with `${1}`.
       """).format(_MEMBER_NAME_IDENTIFIER, _SERVER_NAME_IDENTIFIER).strip()
+
+   DEFAULT_SETTINGS = {
+      "greeting_template": "Hey, $memname! Welcome to $servername! :)",
+   }
 
    async def _initialize(self, resources):
       self._res = resources
       self._client = self._res.client
 
-      self._greeting_template = "YO DAWG"
+      self._greeting_template = None # To be loaded.
+
+      self._load_settings()
 
       self._res.suppress_autokill(True)
+      return
+
+   def _load_settings(self):
+      settings = self._res.get_settings()
+      if settings is None:
+         self._res.save_settings(self.DEFAULT_SETTINGS)
+         settings = self._res.get_settings()
+      try:
+         self._greeting_template = settings["greeting_template"]
+      except KeyError:
+         self._greeting_template = self.DEFAULT_SETTINGS["greeting_template"]
+      return
+
+   def _save_settings(self):
+      settings = {"greeting_template": self._greeting_template}
+      self._res.save_settings(settings)
       return
 
    def get_help_detail(self, substr, privilege_level, module_alias):
@@ -52,8 +74,8 @@ See `{modhelp}` to manage the new-member PM greeting system.
    @cmd.add(_cmd_dict, "view", "viewmessage", "see", "get", "getmessage")
    async def _cmdf_view(self, substr, msg, privilege_level):
       """`{cmd}` - View a copy of the server greeting message."""
-      buf = "**New members are sent the following:**\n"
-      buf += self._get_server_greeting(msg.author.name)
+      buf = "**New members are sent the following (with appropriate substitutions):**\n"
+      buf += self._greeting_template
       await self._client.send_msg(msg, buf)
       return
 
@@ -61,7 +83,7 @@ See `{modhelp}` to manage the new-member PM greeting system.
    async def _cmdf_viewmono(self, substr, msg, privilege_level):
       """`{cmd}` - View a monospace copy of the server greeting message."""
       buf = "**Monospace copy of the server greeting message:**\n```\n"
-      buf += self._get_server_greeting(msg.author.name) + "\n```"
+      buf += self._greeting_template + "\n```"
       await self._client.send_msg(msg, buf)
       return
 
@@ -87,17 +109,18 @@ See `{modhelp}` to manage the new-member PM greeting system.
          await self._client.send_msg(msg, "Error: Message is too long.")
          return
       self._greeting_template = substr
+      self._save_settings()
       await self._client.send_msg(msg, "Successfully set the new greeting PM. Please double-check.")
       return
 
    async def on_member_join(self, member):
-      buf = self._get_server_greeting(member.name) # Note: Errors will propagate out.
+      buf = self._get_server_greeting(member) # Note: Errors will propagate out.
       await self._client.send_msg(member, buf)
       return
 
-   def _get_server_greeting(self, new_member_name):
+   def _get_server_greeting(self, new_member):
       mapping = {
-         self._MEMBER_NAME_IDENTIFIER: new_member_name,
+         self._MEMBER_NAME_IDENTIFIER: "<@{}>".format(new_member.id),
          self._SERVER_NAME_IDENTIFIER: self._res.server.name,
       }
       return string.Template(self._greeting_template).safe_substitute(mapping)
