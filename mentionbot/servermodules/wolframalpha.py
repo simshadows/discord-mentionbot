c2cd7cfd90fe8652d5ed2e67f3b250859928f8db
Rhomil Cabanilla <rhomilc@hotmail.com>
2016-09-04 05:52:13 +1000

    Fixed UnboundLocalError issue on wolfram alpha's "define" command.

--- mentionbot/servermodules/wolframalpha.py
+++ mentionbot/servermodules/wolframalpha.py
@@ -104,12 +104,12 @@ class WolframAlpha(ServerModule):
          return
 
       result = await self.wa_query("define " + substr, msg.channel)
+      pods = list(result.pods)
 
       if len(pods) <= 1:
          await self._client.send_msg(msg, "No result.")
          return
-
-      pods = list(result.pods)
+      
       buf = "**" + pods[0].text + "**\n" + pods[1].text
       if len(buf) == 0:
          raise Exception
