593e3e28af554e747f2b81fe08613a6a28a85447
simshadows <contact@simshadows.com>
2023-01-14 23:26:33 +1100

    Added various features

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simbot
+
+A small Discord bot, hardcoded to only work on my servers.
+
+*I might update it to be general-purpose, but not now.*
+
+
+## Running this bot
+
+1) [Install Rust tooling.](https://www.rust-lang.org/tools/install)
+
+    - *(Rust isn't in a lot of package repositories yet, so you may need to download an installer/script.)*
+
+2) `export DISCORD_TOKEN=yourtokenhere`
+
+3) `cargo run`
+
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,58 @@ use std::env;
 use serenity::async_trait;
 use serenity::prelude::*;
 use serenity::model::channel::Message;
-use serenity::framework::standard::macros::{command, group};
+use serenity::model::gateway::{Ready, Activity};
+use serenity::framework::standard::macros::{command, group, hook};
 use serenity::framework::standard::{StandardFramework, CommandResult};
 
 #[group]
-#[commands(ping)]
+#[commands(
+    help,
+    ping,
+    servers,
+)]
 struct General;
 
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let channel_name = match msg.channel_id.name(ctx.cache).await {
+            Some(s) => s,
+            None => "(no channel name)".to_string(),
+        };
+        println!("#{} [{}#{}]: {}", channel_name, msg.author.name, msg.author.discriminator, msg.content);
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        let http = ctx.http.clone();
+
+        println!("Username: {}", ready.user.name);
+        println!("Shard ID: {}", ctx.shard_id);
+
+        match http.get_guilds(None, None).await {
+            Ok(guilds) => {
+                for guild in guilds.iter() {
+                    println!("Found guild: {}", guild.name);
+                }
+            },
+            Err(e) => {
+                println!("'get_guilds' Error: {:?}", e);
+            }
+        };
+
+        ctx.shard.set_activity(Some(Activity::watching("u sleep")));
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .configure(|c| c.prefix("!"))
+        .unrecognised_command(unrecognized_command_hook)
         .group(&GENERAL_GROUP);
 
-    // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
@@ -30,10 +63,32 @@ async fn main() {
         .await
         .expect("Error creating client");
 
-    // start listening for events by starting a single shard
+    // Start a single shard
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
+}
+
+#[hook]
+async fn unrecognized_command_hook(_: &Context, _: &Message, cmd_name: &str) {
+    println!("Unrecognized command: {}", cmd_name);
+}
+
+#[command]
+async fn help(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Not yet implemented.").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn servers(ctx: &Context, msg: &Message) -> CommandResult {
+    let http = ctx.http.clone();
+    let guilds = http.get_guilds(None, None).await?;
+    println!("guilds: {:?}", guilds);
+    msg.reply(ctx, format!("{:?}", guilds)).await?;
+
+    Ok(())
 }
 
 #[command]
